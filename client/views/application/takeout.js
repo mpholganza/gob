433ca15e38345b00ad154e1bbeaebdc867968274
Paolo Holganza <paolo.holganza@gmail.com>
2014-07-03 17:32:05 -0400

    add privacy policy and terms of service. promo code form input. empty email address check on landing page

--- client/views/application/takeout.js
+++ client/views/application/takeout.js
@@ -3,7 +3,19 @@ Template.takeout.events({
     e.preventDefault();
     //Template._loginButtons.toggleDropdown();
 		var email = $(e.target).find('[name=email]').val();
+    if (email === null || email === '') {
+      $('#join-now-errors').show();
+    }
     Router.go('accountCreate', {email: email});
+  },
+  'keypress #emailInput': function(e) {
+    $('#join-now-errors').hide();
+  },
+  'click #terms-link': function(e) {
+    Router.go('terms');
+  },
+  'click #privacy-link': function(e) {
+    Router.go('privacy');
   }
 });
 
