b904416008e0b5ddd20e3e3a28a8d491063cb782
Paolo Holganza <paolo.holganza@gmail.com>
2014-08-25 16:32:31 -0400

    collections/orders.js

--- server/Accounts.js
+++ server/Accounts.js
@@ -1,7 +1,3 @@
-Accounts.config({
-  forbidClientAccountCreation:true
-});
-
 Accounts.validateNewUser(function(user) {
   // Validate email
   emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
